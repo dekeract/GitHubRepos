77fdae68bf6fcab8d8191d9f586360a3f34ef9d6
Dmitriy Nikolayenko <dima.nik.dev@gmail.com>
2016-10-06 18:41:36 +0300

    Finish project

---
+++ GitHubReps/Repo.swift
@@ -0,0 +1,9 @@
+//
+//  Repo.swift
+//  GitHubReps
+//
+//  Created by Dima Nikolayenko on 10/6/16.
+//  Copyright © 2016 Dima Nikolayenko. All rights reserved.
+//
+
+import Foundation
---
+++ GitHubReps/RepoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RepoTableViewCell.swift
+//  GitHubReps
+//
+//  Created by Dima Nikolayenko on 10/6/16.
+//  Copyright © 2016 Dima Nikolayenko. All rights reserved.
+//
+
+import UIKit
+
+class RepoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
